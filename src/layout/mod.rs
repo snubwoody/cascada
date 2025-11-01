b7f066402e99e8b977394015917305ad741037b1
waku <wakunguma13@gmail.com>
2025-11-01 17:07:08 -0600

    Restructure codebase

---
+++ src/layout/mod.rs
@@ -0,0 +1,137 @@
+use std::fmt::Debug;
+use crate::{Bounds, BoxConstraints, GlobalId, IntrinsicSize, LayoutError, Position, Size};
+
+pub mod block;
+pub mod empty;
+pub mod horizontal;
+pub mod vertical;
+
+pub use block::BlockLayout;
+pub use empty::EmptyLayout;
+pub use horizontal::HorizontalLayout;
+pub use vertical::VerticalLayout;
+
+/// Solve the final size and position of all the layout nodes. The
+/// `window_size` is the maximum available space for the root node.
+///
+/// This functions
+/// returns any layout errors such as overflow or out of bounds.
+///
+/// # Example
+///
+/// ```
+/// use cascada::{solve_layout, BlockLayout, EmptyLayout, IntrinsicSize, Padding, Size};
+///
+/// let child = EmptyLayout::new()
+///     .intrinsic_size(IntrinsicSize::fixed(50.0,50.0));
+/// let mut block = BlockLayout::new(child)
+///     .padding(Padding::all(10.0));
+///
+/// let errors = solve_layout(&mut block,Size::unit(500.0));
+/// assert!(errors.is_empty());
+/// ```
+pub fn solve_layout(root: &mut dyn Layout, window_size: Size) -> Vec<LayoutError> {
+    root.set_max_width(window_size.width);
+    root.set_max_height(window_size.height);
+
+    // It's important that the min constraints are solved before the max constraints
+    // because the min constraints are used in calculating max constraints.
+    let _ = root.solve_min_constraints();
+    root.solve_max_constraints(window_size);
+    root.update_size();
+    root.position_children();
+
+    root.collect_errors()
+}
+
+/// A layout node.
+pub trait Layout: Debug + private::Sealed {
+    fn label(&self) -> String;
+
+    /// Solve the minimum constraints of each [`Layout`] node recursively
+    fn solve_min_constraints(&mut self) -> (f32, f32);
+
+    /// Solve the max constraints for the children and pass them down the tree
+    fn solve_max_constraints(&mut self, space: Size);
+
+    /// Position the layout nodes after size calculations.
+    fn position_children(&mut self);
+
+    /// Update the size of every [`LayoutNode`] based on it's size and constraints.
+    fn update_size(&mut self);
+
+    /// Collect all the errors from the node tree.
+    fn collect_errors(&mut self) -> Vec<LayoutError>;
+
+    /// Get the `id` of the [`Layout`]
+    fn id(&self) -> GlobalId;
+
+    /// Get the [`BoxConstraints`] of the [`Layout`]
+    fn constraints(&self) -> BoxConstraints;
+
+    /// Get the [`IntrinsicSize`] of the [`Layout`]
+    fn get_intrinsic_size(&self) -> IntrinsicSize;
+
+    /// Get the `Size` of the [`Layout`]
+    fn size(&self) -> Size;
+
+    /// Get the `Position` of the [`Layout`]
+    fn position(&self) -> Position;
+
+    /// Get the `Bounds` of the [`Layout`]
+    fn bounds(&self) -> Bounds {
+        Bounds::new(self.position(), self.size())
+    }
+
+    fn children(&self) -> &[Box<dyn Layout>];
+
+    fn set_max_width(&mut self, width: f32);
+    fn set_max_height(&mut self, height: f32);
+    fn set_min_width(&mut self, width: f32);
+    fn set_min_height(&mut self, height: f32);
+
+    fn set_position(&mut self, position: Position) {
+        self.set_x(position.x);
+        self.set_y(position.y);
+    }
+
+    fn set_x(&mut self, x: f32);
+    fn set_y(&mut self, y: f32);
+
+    /// Iterate over the layout tree.
+    fn iter(&self) -> LayoutIter<'_>;
+
+    /// Get a [`Layout`] by it's `id`.
+    fn get(&self, id: GlobalId) -> Option<&dyn Layout> {
+        self.iter().find(|&layout| layout.id() == id)
+    }
+}
+
+mod private {
+    pub trait Sealed {}
+
+    impl Sealed for super::EmptyLayout {}
+    impl Sealed for super::BlockLayout {}
+    impl Sealed for super::HorizontalLayout {}
+    impl Sealed for super::VerticalLayout {}
+}
+
+/// An `Iterator` over the layout tree.
+pub struct LayoutIter<'a> {
+    stack: Vec<&'a dyn Layout>,
+}
+
+impl<'a> Iterator for LayoutIter<'a> {
+    type Item = &'a dyn Layout;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(layout) = self.stack.pop() {
+            let children = layout.children();
+            let m = children.iter().map(|child| child.as_ref());
+            self.stack.extend(m.rev());
+            return Some(layout);
+        }
+
+        None
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -87,164 +87,23 @@
 #![warn(clippy::suboptimal_flops)]
 #![warn(clippy::suspicious_operation_groupings)]
 #![warn(clippy::imprecise_flops)]
-pub mod block;
 #[cfg(feature = "debug-tools")]
 pub mod debug;
-pub mod empty;
 mod error;
-pub mod horizontal;
 mod position;
 mod size;
-pub mod vertical;
+mod layout;
+mod constraints;
 
-pub use block::BlockLayout;
-pub use empty::EmptyLayout;
+pub use layout::*;
+pub use constraints::*;
 pub use error::LayoutError;
-pub use horizontal::HorizontalLayout;
 pub use position::Bounds;
 pub use position::Position;
 pub use size::Size;
 use std::fmt::Debug;
 use std::sync::atomic::{AtomicU32, Ordering};
-pub use vertical::VerticalLayout;
 
-/// Solve the final size and position of all the layout nodes. The
-/// `window_size` is the maximum available space for the root node.
-///
-/// This functions
-/// returns any layout errors such as overflow or out of bounds.
-///
-/// # Example
-///
-/// ```
-/// use cascada::{solve_layout, BlockLayout, EmptyLayout, IntrinsicSize, Padding, Size};
-///
-/// let child = EmptyLayout::new()
-///     .intrinsic_size(IntrinsicSize::fixed(50.0,50.0));
-/// let mut block = BlockLayout::new(child)
-///     .padding(Padding::all(10.0));
-///
-/// solve_layout(&mut block,Size::unit(500.0));
-/// ```
-pub fn solve_layout(root: &mut dyn Layout, window_size: Size) -> Vec<LayoutError> {
-    root.set_max_width(window_size.width);
-    root.set_max_height(window_size.height);
-
-    // It's important that the min constraints are solved before the max constraints
-    // because the min constraints are used in calculating max constraints.
-    let _ = root.solve_min_constraints();
-    root.solve_max_constraints(window_size);
-    root.update_size();
-    root.position_children();
-
-    root.collect_errors()
-}
-
-/// A layout node.
-pub trait Layout: Debug + private::Sealed {
-    fn label(&self) -> String;
-
-    /// Solve the minimum constraints of each [`Layout`] node recursively
-    fn solve_min_constraints(&mut self) -> (f32, f32);
-
-    /// Solve the max constraints for the children and pass them down the tree
-    fn solve_max_constraints(&mut self, space: Size);
-
-    /// Position the layout nodes after size calculations.
-    fn position_children(&mut self);
-
-    /// Update the size of every [`LayoutNode`] based on it's size and constraints.
-    fn update_size(&mut self);
-
-    /// Collect all the errors from the node tree.
-    fn collect_errors(&mut self) -> Vec<LayoutError>;
-
-    /// Get the `id` of the [`Layout`]
-    fn id(&self) -> GlobalId;
-
-    /// Get the [`BoxConstraints`] of the [`Layout`]
-    fn constraints(&self) -> BoxConstraints;
-
-    /// Get the [`IntrinsicSize`] of the [`Layout`]
-    fn get_intrinsic_size(&self) -> IntrinsicSize;
-
-    /// Get the `Size` of the [`Layout`]
-    fn size(&self) -> Size;
-
-    /// Get the `Position` of the [`Layout`]
-    fn position(&self) -> Position;
-
-    /// Get the `Bounds` of the [`Layout`]
-    fn bounds(&self) -> Bounds {
-        Bounds::new(self.position(), self.size())
-    }
-
-    fn children(&self) -> &[Box<dyn Layout>];
-
-    fn set_max_width(&mut self, width: f32);
-    fn set_max_height(&mut self, height: f32);
-    fn set_min_width(&mut self, width: f32);
-    fn set_min_height(&mut self, height: f32);
-
-    fn set_position(&mut self, position: Position) {
-        self.set_x(position.x);
-        self.set_y(position.y);
-    }
-
-    fn set_x(&mut self, x: f32);
-    fn set_y(&mut self, y: f32);
-
-    /// Iterate over the layout tree.
-    fn iter(&self) -> LayoutIter<'_>;
-
-    /// Get a [`Layout`] by it's `id`.
-    fn get(&self, id: GlobalId) -> Option<&dyn Layout> {
-        self.iter().find(|&layout| layout.id() == id)
-    }
-}
-
-mod private {
-    pub trait Sealed {}
-
-    impl Sealed for super::EmptyLayout {}
-    impl Sealed for super::BlockLayout {}
-    impl Sealed for super::HorizontalLayout {}
-    impl Sealed for super::VerticalLayout {}
-}
-
-/// An `Iterator` over the layout tree.
-pub struct LayoutIter<'a> {
-    stack: Vec<&'a dyn Layout>,
-}
-
-impl<'a> Iterator for LayoutIter<'a> {
-    type Item = &'a dyn Layout;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(layout) = self.stack.pop() {
-            let children = layout.children();
-            let m = children.iter().map(|child| child.as_ref());
-            self.stack.extend(m.rev());
-            return Some(layout);
-        }
-
-        None
-    }
-}
-
-/// Describes the size a [`Layout`] will try to be.
-#[derive(Debug, Clone, Copy, Default, PartialEq, PartialOrd)]
-pub enum BoxSizing {
-    /// The [`Layout`] will be a fixed size regardless of any other conditions, this can
-    /// cause overflow if not used wisely.
-    Fixed(f32),
-    /// Tries to be as small as possible
-    #[default]
-    Shrink,
-    /// Tries to be as big as possible, the behaviour of the flex factor is
-    /// dependent on the type of layout.
-    Flex(u8),
-}
 
 static COUNTER: AtomicU32 = AtomicU32::new(0);
 
@@ -280,125 +139,6 @@ pub enum AxisAlignment {
     Center,
     /// Place content at the end.
     End,
-}
-
-/// Describes the maximum and minimum size of a [`Layout`].
-#[derive(Debug, Clone, Copy, Default, PartialEq, PartialOrd)]
-pub struct BoxConstraints {
-    /// The maximum possible width.
-    pub max_width: f32,
-    /// The maximum possible height.
-    pub max_height: f32,
-    /// The minimum possible height.
-    pub min_height: f32,
-    /// The minimum possible width.
-    pub min_width: f32,
-}
-
-impl BoxConstraints {
-    /// Create new [`BoxConstraints`].
-    pub const fn new() -> Self {
-        Self {
-            max_height: 0.0,
-            max_width: 0.0,
-            min_height: 0.0,
-            min_width: 0.0,
-        }
-    }
-}
-
-/// This is the preferred size of a [`Layout`] node.
-#[derive(Debug, Clone, Copy, Default, PartialEq, PartialOrd)]
-pub struct IntrinsicSize {
-    /// The intrinsic width.
-    pub width: BoxSizing,
-    /// The intrinsic height.
-    pub height: BoxSizing,
-}
-
-impl IntrinsicSize {
-    /// Create an intrinsic size with a flex factor of `1`.
-    ///
-    /// # Example
-    /// ```
-    /// use cascada::{BoxSizing,IntrinsicSize};
-    ///
-    /// let instrinsic_size = IntrinsicSize::fill();
-    ///
-    /// assert_eq!(instrinsic_size.width,BoxSizing::Flex(1));
-    /// assert_eq!(instrinsic_size.height,BoxSizing::Flex(1));
-    /// ```
-    pub const fn fill() -> Self {
-        Self {
-            width: BoxSizing::Flex(1),
-            height: BoxSizing::Flex(1),
-        }
-    }
-
-    /// Creates an intrinsic size with a flex factor.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use cascada::{BoxSizing, IntrinsicSize};
-    ///
-    /// let intrinsic_size = IntrinsicSize::flex(8);
-    ///
-    /// assert_eq!(intrinsic_size.width,BoxSizing::Flex(8));
-    /// assert_eq!(intrinsic_size.height,BoxSizing::Flex(8));
-    /// ```
-    pub const fn flex(factor: u8) -> Self {
-        Self {
-            width: BoxSizing::Flex(factor),
-            height: BoxSizing::Flex(factor),
-        }
-    }
-
-    /// Creates an [`IntrinsicSize`] that shrinks to fit its contents.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// use cascada::{BoxSizing, IntrinsicSize};
-    ///
-    /// let intrinsic_size = IntrinsicSize::shrink();
-    ///
-    /// assert_eq!(intrinsic_size.width, BoxSizing::Shrink);
-    /// assert_eq!(intrinsic_size.height, BoxSizing::Shrink);
-    /// ```
-    pub const fn shrink() -> Self {
-        Self {
-            width: BoxSizing::Shrink,
-            height: BoxSizing::Shrink,
-        }
-    }
-
-    /// Creates an [`IntrinsicSize`] with a fixed size.
-    ///
-    /// # Example
-    /// ```
-    /// use cascada::{IntrinsicSize,BoxSizing};
-    ///
-    /// let intrinsic_size = IntrinsicSize::fixed(100.0,50.0);
-    ///
-    /// assert_eq!(intrinsic_size.width,BoxSizing::Fixed(100.0));
-    /// assert_eq!(intrinsic_size.height,BoxSizing::Fixed(50.0));
-    /// ```
-    pub const fn fixed(width: f32, height: f32) -> Self {
-        Self {
-            width: BoxSizing::Fixed(width),
-            height: BoxSizing::Fixed(height),
-        }
-    }
-}
-
-impl From<Size> for IntrinsicSize {
-    fn from(size: Size) -> Self {
-        IntrinsicSize {
-            width: BoxSizing::Fixed(size.width),
-            height: BoxSizing::Fixed(size.height),
-        }
-    }
 }
 
 /// The space between the edges of a [`Layout`] node and its content.
