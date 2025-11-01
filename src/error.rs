0a0085affb5f6d5ee002bf9e77fe9c4aae8992bf
Wakunguma K. <67170475+snubwoody@users.noreply.github.com>
2025-11-01 16:42:48 -0600

    Remove thiserror dependency (#28)

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,4 @@
 use crate::GlobalId;
-use thiserror::Error;
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub enum OverflowAxis {
@@ -13,16 +12,16 @@ impl std::fmt::Display for OverflowAxis {
     }
 }
 
-#[derive(Debug, Clone, Error, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum LayoutError {
-    #[error("Widget(id:{child_id}) is out of it's parent's (id:{parent_id}) bounds")]
     OutOfBounds {
         parent_id: GlobalId,
         child_id: GlobalId,
     },
-
-    #[error("Widget(id:{id})'s children have overflown in the {axis}")]
-    Overflow { id: GlobalId, axis: OverflowAxis },
+    Overflow {
+        id: GlobalId,
+        axis: OverflowAxis,
+    },
 }
 
 impl LayoutError {
@@ -35,5 +34,24 @@ impl LayoutError {
 
     pub fn overflow(id: GlobalId, axis: OverflowAxis) -> Self {
         Self::Overflow { id, axis }
+    }
+}
+
+impl std::error::Error for LayoutError {}
+
+impl std::fmt::Display for LayoutError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            Self::OutOfBounds {
+                parent_id,
+                child_id,
+            } => write!(
+                f,
+                "Widget(id:{child_id}) is out of it's parent's (id:{parent_id}) bounds"
+            ),
+            Self::Overflow { id, axis } => {
+                write!(f, "Widget(id:{id})'s children have overflown in the {axis}")
+            }
+        }
     }
 }
