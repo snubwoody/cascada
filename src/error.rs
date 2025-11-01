525e3d8ac1c15b45be61d5e2c3642997738380c3
waku <wakunguma13@gmail.com>
2025-11-01 16:39:21 -0600

    Run cargo fmt

--- src/error.rs
+++ src/error.rs
@@ -12,13 +12,16 @@ impl std::fmt::Display for OverflowAxis {
     }
 }
 
-#[derive(Debug, Clone,  PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum LayoutError {
     OutOfBounds {
         parent_id: GlobalId,
         child_id: GlobalId,
     },
-    Overflow { id: GlobalId, axis: OverflowAxis },
+    Overflow {
+        id: GlobalId,
+        axis: OverflowAxis,
+    },
 }
 
 impl LayoutError {
@@ -39,10 +42,16 @@ impl std::error::Error for LayoutError {}
 impl std::fmt::Display for LayoutError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self {
-            Self::OutOfBounds { parent_id, child_id } =>
-                write!(f,"Widget(id:{child_id}) is out of it's parent's (id:{parent_id}) bounds"),
-            Self::Overflow { id, axis } =>
-                write!(f,"Widget(id:{id})'s children have overflown in the {axis}")
+            Self::OutOfBounds {
+                parent_id,
+                child_id,
+            } => write!(
+                f,
+                "Widget(id:{child_id}) is out of it's parent's (id:{parent_id}) bounds"
+            ),
+            Self::Overflow { id, axis } => {
+                write!(f, "Widget(id:{id})'s children have overflown in the {axis}")
+            }
         }
     }
 }
