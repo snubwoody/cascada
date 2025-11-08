330dd0d21dc963389365df0dfa7129210ecac3d9
waku <wakunguma13@gmail.com>
2025-11-08 08:10:27 -0700

    add more ffi

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,7 +1,7 @@
 use crate::EmptyLayout;
 
 #[derive(Debug,Clone,Copy,PartialEq,Eq,PartialOrd,Ord)]
-#[repr(C)]
+#[repr(u8)]
 pub enum LayoutKind{
     Empty = 0,
     Block = 1,
@@ -9,11 +9,29 @@ pub enum LayoutKind{
     Vertical = 3,
 }
 
+#[derive(Debug,Clone,Copy,PartialEq,Eq,PartialOrd,Ord)]
+#[repr(u8)]
+pub enum BoxSizingKind{
+    Shrink = 0,
+    Flex = 1,
+    Fixed = 2,
+}
+
+/// A description for creating an [`IntrinsicSize`].
+#[derive(Debug,Clone,Copy)]
+#[repr(C)]
+pub struct IntrinsicValue{
+    kind: BoxSizingKind,
+    value: f32
+}
+
 #[derive(Debug, Clone, Copy)]
 #[repr(C)]
 pub struct LayoutDesc{
     id: u32,
     kind: LayoutKind,
+    intrinsic_width: IntrinsicValue,
+    intrinsic_height: IntrinsicValue,
 }
 
 #[unsafe(no_mangle)]
@@ -22,6 +40,6 @@ pub unsafe extern "C" fn solve_layout_from_desc(desc: LayoutDesc){
 }
 
 fn to_empty_layout(desc: LayoutDesc){
-    EmptyLayout::new()
-        .intrinsic_size();
+    dbg!(desc);
+    let a = EmptyLayout::new();
 }
