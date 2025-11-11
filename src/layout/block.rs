aa847280fa83de74c69168a42e181e4335f85a31
waku <wakunguma13@gmail.com>
2025-11-10 19:03:41 -0700

    fix: set max constraints of fixed root layout
    Previously if the root layout node had a fixed size it's max constraints were still set to the window size. This was causing very subtle bugs.

--- src/layout/block.rs
+++ src/layout/block.rs
@@ -236,8 +236,9 @@ impl Layout for BlockLayout {
         )
     }
 
-    fn solve_max_constraints(&mut self, space: Size) {
-        let mut available_space = space;
+    fn solve_max_constraints(&mut self, _: Size) {
+        let mut available_space =
+            Size::new(self.constraints.max_width.unwrap_or_default(),self.constraints.max_height);
         available_space.width -= self.padding.horizontal_sum();
         available_space.height -= self.padding.vertical_sum();
 
@@ -356,7 +357,7 @@ mod test {
         let layout = EmptyLayout::new().intrinsic_size(IntrinsicSize::fill());
 
         let mut layout = BlockLayout::new(layout);
-        layout.solve_max_constraints(Size::new(100.0, 200.0));
+        solve_layout(&mut layout,Size::new(100.0,200.0));
         assert_eq!(layout.child.constraints().max_width.unwrap(), 100.0);
         assert_eq!(layout.child.constraints().max_height, 200.0);
     }
@@ -367,7 +368,7 @@ mod test {
 
         let mut layout = BlockLayout::new(layout);
         layout.padding = Padding::new(10.0, 15.0, 20.0, 25.0);
-        layout.solve_max_constraints(Size::new(100.0, 200.0));
+        solve_layout(&mut layout,Size::new(100.0,200.0));
         assert_eq!(layout.child.constraints().max_width.unwrap(), 100.0 - 25.0);
         assert_eq!(layout.child.constraints().max_height, 200.0 - 45.0);
     }
